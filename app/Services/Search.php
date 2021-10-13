b2bac7279fc6db7b761d43fadfcf8703300d02e9
Math2121 <matheusdepaula527@gmail.com>
2021-10-12 22:13:49 -0300

    add new change

--- app/Services/Search.php
+++ app/Services/Search.php
@@ -14,20 +14,37 @@ class Search
         $newArray = array_map(function ($item) {
             return array(
                 'Hotel' => $item[0],
-                'Latitude' => $item[1],
-                'Longitude' => $item[2],
+                'Latitude' => floatval($item[1]),
+                'Longitude' => floatval($item[2]),
                 'Price' => number_format($item[3], 2),
             );
         }, $url);
 
         $new_test = array();
+        $lat =  1.28210155945393;
+        $long = 103.81722480263163;
         foreach ($newArray as $item) {
-            if ((intval($item['Latitude']) - 34.5918855804444125) > 0.00200 && (intval($item['Longitude']) - -0.228499) > 0.00200) {
+            if ($this->distance($lat, $long, $item['Latitude'], $item['Longitude'])) {
                 array_push($new_test, $item);
             }
         }
         return $new_test;
     }
 
-   
+    public function distance($lat1, $lon1, $lat2, $lon2)
+    {
+        if (($lat1 == $lat2) && ($lon1 == $lon2)) {
+            return 0;
+        } else {
+            $theta = $lon1 - $lon2;
+            $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+            $dist = acos($dist);
+            $dist = rad2deg($dist);
+            $miles = $dist * 60 * 1.1515;
+
+
+
+            return ($miles * 1.609344);
+        }
+    }
 }
